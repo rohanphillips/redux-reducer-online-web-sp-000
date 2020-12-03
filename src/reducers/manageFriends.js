7608c917801700f400cecd8dc88e770c2e21788c
rohanphillips <rohan@solutions-insurance.com>
2020-12-03 11:53:54 -0600

    commit tree 889322839af8a7beab32f3d080860ce2f4262fb1

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+  switch (action.type){
+    case "ADD_FRIEND":
+      state.friends.push(action.friend);
+      return {friends: state.friends};
+    case "REMOVE_FRIEND":
+      let removed = state.friends.filter(function(value, index, arr){
+        return value.id != action.id
+      })
+      return {friends: removed}
+    default:
+      return state;
+  }
 }
